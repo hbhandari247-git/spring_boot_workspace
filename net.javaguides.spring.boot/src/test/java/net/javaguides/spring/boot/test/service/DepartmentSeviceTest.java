970ca15955f7e488ca13b41d6909a72ea57638c7
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-05-31 09:07:52 +0530

    "Updating junit test case"

--- net.javaguides.spring.boot/src/test/java/net/javaguides/spring/boot/test/service/DepartmentSeviceTest.java
+++ net.javaguides.spring.boot/src/test/java/net/javaguides/spring/boot/test/service/DepartmentSeviceTest.java
@@ -3,6 +3,7 @@ package net.javaguides.spring.boot.test.service;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -31,10 +32,15 @@ public class DepartmentSeviceTest {
 		departmentEntity.setDepartmentName("BIO");
 		departmentEntity.setDepartmentAddress("A-Block");
 
+		DepartmentEntity departmentEntity4 = new DepartmentEntity();
+		
 		when(departmentRepository.findById(1L)).thenReturn(Optional.of(departmentEntity));
+		when(departmentRepository.findById(2L)).thenReturn(Optional.of(departmentEntity4));
 
 		Optional<DepartmentEntity> departmentEntity2 = departmentService.fetchDepartmentById(1L);
+		Optional<DepartmentEntity> departmentEntity3 = departmentService.fetchDepartmentById(2L);
 		assertEquals(1L, departmentEntity2.get().getDepartmentId());
 		assertEquals("BIO", departmentEntity2.get().getDepartmentName());
+		assertNotEquals(1L, departmentEntity3.get().getDepartmentId());
 	}
 }
